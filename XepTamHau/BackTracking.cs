28daecb8394360da20adca0a001635012b24e49d
hoadinh216 <dinhthihoa.mta@gmail.com>
2017-05-29 13:50:41 +0700

    Bai toan 8 hau viet bang visual studio C_Sharp

---
+++ XepTamHau/BackTracking.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XepTamHau
+{
+    public class BackTracking
+    {
+        private int[] solution;
+        private int N;
+        private BackTracking() { }
+        public BackTracking(int N)
+        {
+            solution = new int[N];
+            this.N = N;
+            int i;
+            for (i = 0; i < N; i++)
+                solution[i] = 0;
+        }
+        int count = 0;
+        public void show(int N)
+        {
+            int i;
+            Console.WriteLine("\n(" + count++ + ")");
+            for (i = 0; i < N; i++)
+                Console.Write("\t" + solution[i]);
+        }
+        public void placeQueens(int row)
+        {
+            int i = 0;
+            for (i = 0; i < N; i++)
+                if (canPlace(row, i))
+                {
+                    solution[row] = i;
+                    if (row == N - 1)
+                        show(N);
+                    placeQueens(row + 1);
+                }
+        }
+        /*
+         x1==x2 means same rows,
+         y1==y2 means same columns
+         |x2-x1|==|y2-y1| means they are placed in diagonals.
+        */
+        public Boolean canPlace(int x2, int y2)
+        {
+            int i;
+            for (i = 0; i < x2; i++)
+            {
+                if (solution[i] == y2 || (Math.Abs(i - x2) == Math.Abs(solution[i] - y2)))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ XepTamHau/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XepTamHau
+{
+    class Program
+    {
+        const int N = 8;
+        //const int TEMP = N - 1;
+        static int[] row = new int[N + 1];//quan hậu thứ i đặt trên cột j: xi= j;
+        static bool[] col = new bool[N+ 1];// aj= 0=> ô thứ j ko còn trống nữa
+        static bool[] toLeft = new bool[2 * N ];//đường chéo b[i+j] không còn trống nữa
+        static bool[] toRigh = new bool[2 * N ];//đường chéo c[i-j] không còn trống nưa
+        static void Main(string[] args)
+        {
+            BackTracking bt = new BackTracking(8);
+            //bt.placeQueens(0);
+            //  int start = 1;
+            init();
+            func_BackTracking(1);
+
+            Console.ReadKey();
+        }
+        private static void init()
+        {
+            int i;
+            for (i = 0; i < col.Length; i++)
+            {
+                row[i] = 0;//hàng i đặt con hậu thứ 0
+                col[i] = true;//chưa có 
+            }
+            for (i = 0; i < toLeft.Length; i++)
+            {
+                toLeft[i] = true;//chưa có 
+                toRigh[i] = true;
+            }
+        }
+
+        private static void func_BackTracking(int start)
+        {
+            int j;
+            for (j = 1; j <= N; j++)
+                if (col[j] && toLeft[start + j-1] && toRigh[start - j + N ])
+                {
+                    row[start] = j;
+                    col[j] = false;
+                    toLeft[start + j-1] = false;
+                    toRigh[start - j + (N )] = false;
+                    if (start <  N )func_BackTracking(start + 1);
+                       
+                    else
+                         showResult();
+                       
+                    col[j] = true;
+                    toLeft[start + j -1] = true;
+                    toRigh[start - j + (N)] = true;
+                }
+        }
+
+        static int n = 0;
+        private static void showResult()
+        {
+            int i;
+            Console.WriteLine("n = " + n++);
+            for (i = 1; i < row.Length; i++)
+                Console.Write("\t" + row[i]);
+            Console.WriteLine();
+        }
+    }
+}
